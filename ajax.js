bbde3e8dcad02ed20013a446a046c8bdf82f9523
musakazim989 <musakazim989@gmail.com>
2023-06-20 23:08:48 +0600

    ajax

---
+++ ajax.js
@@ -0,0 +1,15 @@
+function sendGetRequest() {
+  var xhr = new XMLHttpRequest()
+  var url = "https://api.example.com/data"
+
+  xhr.open("GET", url, true)
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var response = JSON.parse(xhr.responseText)
+      console.log(response)
+    }
+  }
+
+  xhr.send()
+}
